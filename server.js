b26b16ef2d9d90d0bfc7bbcc0cfe85a1b2c05f75
Henok Alemu Addis <henok4@gmail.com>
2015-01-28 01:54:35 +0000

    Slight change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response){
 });
 
 app.get('/nickname', function(request, response){
-    response.send('wild mallard');
+    response.send('wild-mallard');
 });
 
 app.listen(
